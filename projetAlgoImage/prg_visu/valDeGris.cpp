50cfb8b8f33cbde1c8c0c1bd12a3a3e92226c33f
Yaelle <yaellesaraleguitran@gmail.com>
2021-05-17 23:21:57 +0200

    Plus de strilles youpi

--- projetAlgoImage/prg_visu/valDeGris.cpp
+++ projetAlgoImage/prg_visu/valDeGris.cpp
@@ -11,7 +11,7 @@ extern "C"{
 void defineHeight(HeightMap * heightMap) {
 	ifstream lireTexte("../prg_visu/valDeGris.txt");
 		if(lireTexte){
-			lireTexte >> heightMap->h>> heightMap->w;
+			lireTexte >> heightMap->w>> heightMap->h;
 			for (int i = 0; i < heightMap->h; i++){
                 for(int j=0; j<heightMap->w; j++){
                     lireTexte >> heightMap->valeursDeGris[i][j];
